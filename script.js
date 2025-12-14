d851c64c7535d5f4569a5f98f72c3e2c7f4220e2
SHADIL VV <shadilshad508@gmail.com>
2025-12-15 00:23:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hello world!");
+window.alert("welcome buddy");
+let name = "shadil";
+console.log(`the name given is ${name}`);
+
+condition = prompt("how is your condition today?");
+console.log(`the condition of the person is ${condition}`);
+
+function getName(){
+    let realname = document.getElementById("name").value ;
+    console.log(realname);
+}
+
