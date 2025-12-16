d1d4fda8456b475c54b0c1e378e68df54bb6b9a3
SHADIL VV <shadilshad508@gmail.com>
2025-12-16 15:13:40 +0530

    practice

---
+++ practical.js
@@ -0,0 +1,9 @@
+const my_heading = document.getElementById("headinga");
+const new_heading = document.createElement("h2");
+const new_paragraph = document.createElement("p");
+
+my_heading.append(new_heading);
+my_heading.append(new_paragraph);
+
+new_heading.innerHTML= "Hey, Guys! I changed this heading with JavaScript";
+new_paragraph.innerHTML="Best idh ingane aayirunno";
